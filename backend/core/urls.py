751c6d58b56b841fa21c12984e9e37728763599a
george <georgerubinga@gmail.com>
2025-02-16 12:11:24 +0300

    recent1

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from .import views
-
+from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('faq/', views.faq, name='faq'),
+    path('services/', views.services, name='services'),
+    path('rooms/', views.rooms, name='rooms'),
+    path('news/', views.news, name='news'),
+    path('post/', views.post, name='post'),
+    path('restaurant/', views.restaurant, name='restaurant'),
+    path('room-details/', views.room_details, name='room_details'),
+    path('spa-wellness/', views.spa_wellness, name='spa_wellness'),
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,23 +1,34 @@
 from django.shortcuts import render
 
-# Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def about(request):
     return render(request, 'about.html')
 
-def careers(request):
-    return render(request, 'careers.html')
-
 def contact(request):
     return render(request, 'contact.html')
-
-def facilities(request):
-    return render(request, 'facilities.html')
 
 def faq(request):
     return render(request, 'faq.html')
 
-def gallery(request):
-    return render(request, 'gallery.html')
+def services(request):
+    return render(request, 'services.html')
+
+def rooms(request):
+    return render(request, 'rooms.html')
+
+def news(request):
+    return render(request, 'news.html')
+
+def post(request):
+    return render(request, 'post.html')
+
+def restaurant(request):
+    return render(request, 'restaurant.html')
+
+def room_details(request):
+    return render(request, 'room_details.html')
+
+def spa_wellness(request):
+    return render(request, 'spa_wellness.html')
